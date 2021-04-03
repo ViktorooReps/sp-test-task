Fixed entropy calculation

evaluate.py
@@ -201,66 +201,69 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--active", action="store_true")
+    parser.add_argument("--only-plots", action="store_true")
 
     args = parser.parse_args()
 
-    char_to_idx = load_obj("char_to_idx")
-    tag_to_idx = load_obj("tag_to_idx")
-    tok_to_idx = load_obj("tok_to_idx")
+    if not args.only_plots:
+        char_to_idx = load_obj("char_to_idx")
+        tag_to_idx = load_obj("tag_to_idx")
+        tok_to_idx = load_obj("tok_to_idx")
 
-    if not args.active:
-        model = load_obj("model")
-    else:
-        model = load_obj("active_model")
+        if not args.active:
+            model = load_obj("model")
+        else:
+            model = load_obj("active_model")
 
-    data_args = dict(
-        char_to_idx=char_to_idx, 
-        tok_to_idx=tok_to_idx,
-        tag_to_idx=tag_to_idx,
-        max_token_len=max_word_len,
-        preprocessor=preprocess
-    )
-
-    train_seqs = load_obj("train_seqs")
-    train_data = Data(train_seqs, **data_args)
-
-    val_seqs = load_obj("val_seqs")
-    val_data = Data(val_seqs, **data_args)
-
-    test_seqs = load_obj("test_seqs")
-    test_data = Data(test_seqs, **data_args)
-
-    dl_args = dict(
-        batch_size=batch_size,
-        worker_init_fn=seed_worker,
-        pad_collator=PaddingCollator(
-            char_pad=char_to_idx["<pad>"],
-            max_word_len=max_word_len,
-            tok_pad=tok_to_idx["<pad>"],
-            tag_pad=tag_to_idx["O"]
+        data_args = dict(
+            char_to_idx=char_to_idx, 
+            tok_to_idx=tok_to_idx,
+            tag_to_idx=tag_to_idx,
+            max_token_len=max_word_len,
+            preprocessor=preprocess
         )
-    )
 
-    print("\nEvaluating on test set")
-    test_loss, test_f1 = evaluate_model(model, get_eval_dataloader(test_data, **dl_args))
+        train_seqs = load_obj("train_seqs")
+        train_data = Data(train_seqs, **data_args)
 
-    print("Evaluating on train set")
-    train_loss, train_f1 = evaluate_model(model, get_eval_dataloader(train_data, **dl_args))
+        val_seqs = load_obj("val_seqs")
+        val_data = Data(val_seqs, **data_args)
 
-    print("Evaluating on valid set")
-    val_loss, val_f1 = evaluate_model(model, get_eval_dataloader(val_data, **dl_args))
+        test_seqs = load_obj("test_seqs")
+        test_data = Data(test_seqs, **data_args)
 
-    print("\n[test]  loss: " + str(test_loss) + " F1: " + str(test_f1))
-    print("[train] loss: " + str(train_loss) + " F1: " + str(train_f1))
-    print("[valid] loss: " + str(val_loss) + " F1: " + str(val_f1))
+        dl_args = dict(
+            batch_size=batch_size,
+            worker_init_fn=seed_worker,
+            pad_collator=PaddingCollator(
+                char_pad=char_to_idx["<pad>"],
+                max_word_len=max_word_len,
+                tok_pad=tok_to_idx["<pad>"],
+                tag_pad=tag_to_idx["O"]
+            )
+        )
 
-    val_f1s = np.array(load_obj("val_f1_list"))
-    print("Epochs with best F1 scores on validation set:")
-    print(val_f1s.argsort()[::-1])
+        print("\nEvaluating on test set")
+        test_loss, test_f1 = evaluate_model(model, get_eval_dataloader(test_data, **dl_args))
+
+        print("Evaluating on train set")
+        train_loss, train_f1 = evaluate_model(model, get_eval_dataloader(train_data, **dl_args))
+
+        print("Evaluating on valid set")
+        val_loss, val_f1 = evaluate_model(model, get_eval_dataloader(val_data, **dl_args))
+
+        print("\n[test]  loss: " + str(test_loss) + " F1: " + str(test_f1))
+        print("[train] loss: " + str(train_loss) + " F1: " + str(train_f1))
+        print("[valid] loss: " + str(val_loss) + " F1: " + str(val_f1))
+
+        val_f1s = np.array(load_obj("val_f1_list"))
+        print("Epochs with best F1 scores on validation set:")
+        print(val_f1s.argsort()[::-1])
 
     if args.active:
-        plot_active(init=100, step=100)
-        plot_comparison_active(init=100, step=100)
+        plot_active(init=1500, step=100, suff="i1500s100/active_")
+        plot_active(init=1500, step=100, suff="i1500s100/active_", pref="_rand")
+        plot_comparison_active(init=1500, step=100, suff="i1500s100/active_")
     else:
         plot_last_run()
         plot_in_comparison(5)

model/nerc.py
@@ -260,7 +260,7 @@ class EntropyCRF(CRF):
             preds = softmax(topk_scores)
 
             batch_entropy.append(
-                torch.sum(torch.mul(preds, torch.log(preds))).item()
+                - torch.sum(torch.mul(preds, torch.log(preds))).item()
             )
 
         return batch_entropy

train.py
@@ -1,5 +1,5 @@
 from utils.memory_management import limit_memory, load_obj, save_obj
-from utils.plotter import plot_mini
+from utils.plotter import plot_mini, plot_hist, plot_sent_entropies
 from utils.reproducibility import seed_worker, seed
 from model.nerc import *
 from extract import preprocess
@@ -156,25 +156,21 @@ def fit_model_active(model_args, scheduler_args, optimizer_args, inflating_data,
             val_data=val_data, stopper=model_stopper, skip_train_eval=True, verbose=False)
 
         inflating_data.entropy()
+
         if not random_sampling:
-            max_entropy_inds = np.array(evaluate_entropy(
+            new_inds = np.array(evaluate_entropy(
                 model, get_entropy_dataloader(inflating_data, **dl_args)
             )).argsort()[-request_seqs:][::-1]
-
-            for ind in max_entropy_inds:
-                total_labeled += len(inflating_data[ind])
-                total_seqs_added += 1
-
-            inflating_data.add_seqs(max_entropy_inds)
         else:
             sampler = iter(RandomSampler(inflating_data))
-            chosen_indicies = set(next(sampler) for i in range(request_seqs))
+            new_inds = set(next(sampler) for i in range(request_seqs))
 
-            for ind in chosen_indicies:
-                total_labeled += len(inflating_data[ind])
-                total_seqs_added += 1
+        new_labeled = 0
+        for ind in new_inds:
+            new_labeled += len(inflating_data.stored[ind][0])
+            total_seqs_added += 1
 
-            inflating_data.add_seqs(chosen_indicies)
+        inflating_data.add_seqs(new_inds)
 
         inflating_data.eval()
 
@@ -190,6 +186,9 @@ def fit_model_active(model_args, scheduler_args, optimizer_args, inflating_data,
 
         print("[valid] loss: " + str(val_loss) + " F1: " + str(val_f1))
 
+        print("New labeled tokens:", new_labeled)
+
+        total_labeled += new_labeled
         model_num += 1
         stopper.add_epoch(best_model, val_f1)
 
@@ -307,12 +306,12 @@ if __name__ == '__main__':
             pref = ""
 
         save_obj(train_loss_list, suff + "train_loss_list" + pref)
-        save_obj(val_loss_list, "val_loss_list")
+        save_obj(val_loss_list, suff + "val_loss_list" + pref)
 
         save_obj(train_f1_list, suff + "train_f1_list" + pref)
         save_obj(val_f1_list, suff + "val_f1_list" + pref)
 
-        save_obj(best_model, suff + "active_model" + pref)
+        save_obj(best_model, suff + "model" + pref)
         
     else:
         model = CNNbLSTMCRF(char_to_idx, tok_to_idx, tag_to_idx, token_vecs)